8a9715f01832191f07254420f4c6c276a6da23b4
singhalisha829 <singhalisha829@gmail.com>
2022-06-23 15:04:58 +0530

    backdrop

--- components/header.js
+++ components/header.js
@@ -4,23 +4,33 @@ import { useState } from 'react';
 import Backdrop from './backdrop';
 import {FaDropbox ,FaTh,FaSignOutAlt} from 'react-icons/fa';
 import Router from 'next/router';
+import Modal from './modal';
 
 import Image from 'next/image';
+
 
 const Header = () =>{
     const [openNavbar, setOpenNavbar]= useState(false)
     return(
-    <div className="mobile_header">
+    <div className="mobile_header" >
         <div className='hamburger'><FaBars size={25} color="#29394A" onClick={()=>setOpenNavbar(!openNavbar)}/></div>
         <div className='inverted_logo'><Image src={Logo} layout="responsive" /></div>
-        {openNavbar?<div className='mobile_navbar'>
+        
+        <Backdrop show={openNavbar} clicked={()=>setOpenNavbar(false)}/>
+        
+    <div className='mobile_navbar'
+    style={{
+        opacity: openNavbar ? '1' : '0'
+    }}>
             <div className='navbar_items' style={{width:'11rem'}}
             onClick={()=>{Router.push('/');setOpenNavbar(false)}}><FaTh /> Dashboard </div>
             <div className='navbar_items' style={{width:'8rem'}}
             onClick={()=>{Router.push('/order');setOpenNavbar(false)}}><FaDropbox /> Orders </div>
             <div className='navbar_items' style={{width:'8.5rem',position:'fixed',bottom:'2rem'}}
             onClick={()=>{Router.push('/login');setOpenNavbar(false);localStorage.clear()}}><FaSignOutAlt /> Logout </div>
-        </div>:null}
+        </div>
+        
+        
     </div>
     )
 }
