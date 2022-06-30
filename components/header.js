3c0614d213f6edd47c29a18588428b103c0758ed
singhalisha829 <singhalisha829@gmail.com>
2022-06-30 12:57:34 +0530

    mobile suboptions

--- components/header.js
+++ components/header.js
@@ -2,7 +2,9 @@ import { FaBars } from 'react-icons/fa';
 import Logo from "../public/logo.png";
 import { useState,useRef ,useEffect} from 'react';
 
-import {FaDropbox ,FaTh,FaSignOutAlt} from 'react-icons/fa';
+import {FaDropbox ,FaTh,FaSignOutAlt, FaAngleDown, FaAngleUp,FaShoppingCart} from 'react-icons/fa';
+import {IoConstruct} from 'react-icons/io5';
+
 import Router from 'next/router';
 
 
@@ -11,6 +13,8 @@ import Image from 'next/image';
 
 const Header = () =>{
     const [openNavbar, setOpenNavbar]= useState(false);
+    const [showSubOptions,setShowSubOptions] = useState(false);
+
     const wrapperRef = useRef();
   useOutsideAlerter(wrapperRef);
 
@@ -38,9 +42,16 @@ const Header = () =>{
     {openNavbar?<div className='nav_wrapper' >    
     <div className='mobile_navbar' ref={wrapperRef}>
             <div className='navbar_items' style={{width:'11rem'}}
-            onClick={()=>{Router.push('/');setOpenNavbar(false)}}><FaTh /> Dashboard </div>
-            <div className='navbar_items' style={{width:'8rem'}}
-            onClick={()=>{Router.push('/order');setOpenNavbar(false)}}><FaDropbox /> Orders </div>
+            onClick={()=>{Router.push('/');setOpenNavbar(false)}}><FaTh /><div style={{marginLeft:'1rem'}}> Dashboard </div></div>
+            <div className='navbar_items' 
+            onClick={()=>{setShowSubOptions(!showSubOptions)}}><FaDropbox /> <div style={{marginLeft:'1rem'}}>Orders</div>
+            <div className='suboption_icon'>{showSubOptions?<FaAngleUp/>:<FaAngleDown/>}</div> </div>
+
+            {showSubOptions?<div className='header_suboptions'>
+              <div className='header_suboptions_items' onClick={()=>{Router.push('/order');setOpenNavbar(false)}}><div style={{marginRight:'1rem'}}><IoConstruct/></div>Production</div>
+              <div className='header_suboptions_items' onClick={()=>{Router.push('/purchaseOrder');setOpenNavbar(false)}}><div style={{marginRight:'1rem'}}><FaShoppingCart/></div>Purchase</div>
+            </div>:null}
+
             <div className='navbar_items' style={{width:'8.5rem',position:'fixed',bottom:'2rem'}}
             onClick={()=>{Router.push('/login');setOpenNavbar(false);localStorage.clear()}}><FaSignOutAlt /> Logout </div>
         </div>
